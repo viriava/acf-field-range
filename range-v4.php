cd2f94936c4e8f5aa7b7040bac2f2137acacf4d1
viriava <viriava@gmail.com>
2014-09-01 07:57:38 +0200

    version update

--- range-v4.php
+++ range-v4.php
@@ -44,7 +44,7 @@ class acf_field_range extends acf_field
 		$this->settings = array(
 			'path' => apply_filters('acf/helpers/get_path', __FILE__),
 			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
-			'version' => '1.1.2'
+			'version' => '1.1.4'
 		);
 
 	}
